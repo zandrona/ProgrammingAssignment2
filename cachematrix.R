79caadf3e7fdea31eea1b65b8f6e44021d1fc568
zandrona <zandrona@linux.edu>
2015-03-16 20:23:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 # makeCacheMatrix creates a list containing a function to
-# 1. set the value of the vector
-# 2. get the value of the vector
-# 3. set the value of the mean
-# 4. get the value of the mean
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of the inverse
+# 4. get the value of the inverse
 
 
 makeCacheMatrix <- function(x = matrix()) {
